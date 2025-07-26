59b857d0a601f3007bc389ee143aa2a2f984137e
VigneswariV <2311067@nec.edu.in>
2025-07-26 11:28:59 +0530

    Add files via upload

---
+++ push_button_counter_system1.ino
@@ -0,0 +1,23 @@
+const int buttonPin = 2;   // Push button connected to D2
+int counter = 0;
+int buttonState = 0;
+int lastButtonState = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);      // Start Serial Monitor
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  // Rising edge detection (button press)
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    counter++;
+    Serial.print("Button Pressed: ");
+    Serial.println(counter);
+    delay(200); // Debounce delay
+  }
+
+  lastButtonState = buttonState;
+}
